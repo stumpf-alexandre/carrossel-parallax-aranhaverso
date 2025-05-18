40af81c7d9ba60091dc221c556d302cc8a4dbaa6
Alexandre <als.stumpf@gmail.com>
2025-05-18 19:32:23 -0300

    criando o componente de imagem de herói

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import HeroesList from "@/components";
+import HeroesList from "@/components/HeroesList";
 import { IHeroData } from "@/interface/heroes";
 import styles from "./page.module.scss";
 
