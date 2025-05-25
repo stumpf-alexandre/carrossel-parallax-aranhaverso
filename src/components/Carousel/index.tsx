8784fe4503cec1108403fd91221beb52d8b562bd
Alexandre <als.stumpf@gmail.com>
2025-05-25 00:28:21 -0300

    criando e estilizando o componente de informações dos heróis

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,10 +1,16 @@
 import { IHeroData } from "@/interface/heroes"
+import HeroDetails from "../HeroDetails";
 
 interface IProps {
     heroes: IHeroData[];
     activeId: string;
 }
 
-export default function Carousel({ activeId }: IProps) {
-    return <h1>Components Carousel: {activeId}</h1>
+export default function Carousel({ heroes, activeId }: IProps) {
+    return (
+        <>
+            <h1>Components Carousel: {activeId}</h1>
+            <HeroDetails data={heroes[0]} />
+        </>
+    )
 }
